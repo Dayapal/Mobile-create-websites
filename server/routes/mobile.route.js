76516af8c20460877ce710c5d0b7d099f376cf7b
Daya Pal <dayapal12pc@gmail.com>
2026-08-25 11:09:17 +0530

    don change

--- server/routes/mobile.route.js
+++ server/routes/mobile.route.js
@@ -1,9 +1,7 @@
 import express from 'express';
 import { createMobile, deleteMobile, getMobiles, updateMobile } from '../controller/mobile.controller.js';
 import authMiddleware from '../middleware/middleware.js';
-
 export const mobileRoute = express.Router();
-
 mobileRoute.post("/mobile", authMiddleware, createMobile) // CREATE
 mobileRoute.get("/mobiles",   getMobiles) // READ
 mobileRoute.put("/mobile/:id", authMiddleware, updateMobile)
